bd36cf4025dc3ee6918a14abe8c4f8efd0d4cfab
Li, Zhimin <Zhimin.Li@elekta.com>
2018-03-13 18:23:46 +0800

    init tmp folder

---
+++ config.cs
@@ -0,0 +1,24 @@
+
+
+IT password: 
+Password1!
+
+## auto mongod
+mongod --logpath "C:\bdst\log\log.log" --logappend --dbpath "C:\bdst\data" --directoryperdb --serviceName "MongoDB" --serviceDisplayName "MongoDB" --install 
+
+
+## test url 
+https://beamtest.elekta.com/
+account: admin@elekta.com
+password: Elekta20160603
+
+Zhimin.Li@elekta.com
+
+
+training url
+https://elekta.netdimensions.com/elekta/servlet/ekp/TABLISTSESSIONS?DECORATEPAGE=Y#!
+
+outlook 
+
+defect
+http://usjsvtqmapp02/qcbin/start_a.jsp 
---
+++ tmp.cs
@@ -0,0 +1,16 @@
+IT password: 
+Password1!
+
+master: 6a3f28fc
+develop: fe1246ec
+
+3.0: ddcc578
+
+
+merge in develop from release/3.0: 615a4ff8
+
+
+training url
+https://elekta.netdimensions.com/elekta/servlet/ekp/TABLISTSESSIONS?DECORATEPAGE=Y#!
+
+outlook 
